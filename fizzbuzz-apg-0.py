f89dd76defc91dd8f923e1e61ba3074567668387
A. Giddens <AustinGiddensBusiness@gmail.com>
2017-03-24 09:36:20 -0700

    Add files via upload

--- fizzbuzz-apg-0.py
+++ fizzbuzz-apg-0.py
@@ -1,7 +1,10 @@
-for n in range (13,31):
-	if n%3 == 0:
-		print("FIZZ"),
-	if n%5 == 0:
+for n in range (1,101):
+	if n%3 == 0 and n%5 != 0:
+		print("FIZZ")
+	if n%5 == 0 and n%3 != 0:
 		print("BUZZ")
+	if n%3 == 0 and n%5 == 0:
+		print ("FIZZBUZZ")
 	if n%3 != 0 and n%5 != 0:
 		print (n)
+
